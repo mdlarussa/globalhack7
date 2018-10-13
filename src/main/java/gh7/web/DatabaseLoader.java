2f343f2e2e4db10970aa7ed1f662e6632ccce85e
Adam George <awgtek@gmail.com>
2018-10-13 14:30:20 -0500

    add needs

--- src/main/java/gh7/web/DatabaseLoader.java
+++ src/main/java/gh7/web/DatabaseLoader.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
@@ -25,11 +28,16 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        long count = this.immigrantUserRepository.count();
+        long count = this.needRepository.count();
         if (count == 0) {
+            List<String> needStrs = Arrays.asList("Legal", "Medical", "Food", "School", "Transportation", "Recreation", "Housing", "Utilities", "ESL");
+            Need need;
+            for (String needStr : needStrs) {
+                need = this.needRepository.save(new Need(needStr));
+            }
             Immigrant anderson = this.immigrantUserRepository.save(new Immigrant("Anderson", "andy@wahoo.com"));
             this.immigrantUserRepository.save(new Immigrant("Bello", "bello@wahoo.com"));
-            Need need = this.needRepository.save(new Need("To drive a car"));
+            need = this.needRepository.save(new Need("To drive a car"));
             anderson.addNeed(need);
             this.immigrantUserRepository.save(anderson);
         }
